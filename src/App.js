8b4684235aca333810cb76a29a54c3d92a6205de
masahiro <m.togasaki1026@gmail.com>
2025-12-14 16:10:23 +1100

    Made the base for the table tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container'>
+      <table>
+        <thead>
+        <tr>
+          <th>Name</th>
+          <th>Address</th>
+          <th>Phone Number</th>
+          <th>Email</th>
+        </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Jack Michael</td>
+            <td>3 waterfoot road</td>
+            <td>222=444=555</td>
+            <td>JackMichael343@gmail.com</td>
+          </tr>
+        </tbody>
+      </table>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+``
