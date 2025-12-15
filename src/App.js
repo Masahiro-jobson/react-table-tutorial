8038a4f32129513e1113f36df5dec5924d95ebbb
masahiro <m.togasaki1026@gmail.com>
2025-12-15 22:22:08 +1100

    Implement edit, save, cancel, delete buttons with rendering components and props concepts (parent/child)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { nanoid } from "nanoid";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import "./App.css";
+import EditableRow from "./components/EditableRow";
+import ReadOnlyRow from "./components/ReadOnlyRow";
 import data from "./mock-data.json";
 
 function App() {
@@ -12,22 +14,44 @@ function App() {
     fullName: "",
     address: "",
     phoneNumber: "",
-    email: ""
+    email: "",
 
   });
+
+  const [editFormData, setEditFormData] = useState({
+    fullName: "",
+    address: "",
+    phoneNumber: "",
+    email: "",
+  });
+
+
+  const [editContactId, setEditContactId] = useState(null);
 
   const handleAddFormChange = (e) => {
     e.preventDefault();
 
     const fieldName = e.target.getAttribute("name");
     const fieldValue = e.target.value;
-
+    
     // To make a copy of current state of addFormData.
     const newFormData = { ...addFormData};
     // [] is used for the value of the variable fieldName as the key.
     newFormData[fieldName] = fieldValue;
-
+    
     setAddFormData(newFormData);
+  };
+  
+  const handleEditFormChange = (e) => {
+    e.preventDefault();
+    const fieldName = e.target.getAttribute("name");
+    const fieldValue = e.target.value;
+
+    const newFormData = { ...editFormData};
+    newFormData[fieldName] = fieldValue;
+    
+    setEditFormData(newFormData);
+
   };
 
   const handleAddFormSubmit = (e) => {
@@ -46,30 +70,92 @@ function App() {
 
   }
 
+  const handleEditFormSubmit = (e) => {
+    e.preventDefault();
+
+    const editedContact = {
+      id: editContactId,
+      fullName: editFormData.fullName,
+      address: editFormData.address,
+      phoneNumber: editFormData.phoneNumber,
+      email: editFormData.email,
+    }
+
+    const newContacts = [...contacts];
+
+    const index = contacts.findIndex((contact) => contact.id === editContactId)
+
+    newContacts[index] = editedContact;
+
+    setContacts(newContacts);
+    setEditContactId(null);
+
+
+  }
+
+  const handleEditClick = (e, contact) => {
+    e.preventDefault();
+    setEditContactId(contact.id);
+
+    const formValues = {
+      fullName: contact.fullName,
+      address: contact.address,
+      phoneNumber: contact.phoneNumber,
+      email: contact.email,
+    }
+
+    setEditFormData(formValues);
+  }
+
+  const handleCancelClick = () => {
+    setEditContactId(null);
+  }
+
+  const handleDeleteClick = (contactId) => {
+    const newContacts = [...contacts]
+
+    const index = contacts.findIndex((contact)=> contact.id === contactId)
+
+    newContacts.splice(index, 1);
+
+    setContacts(newContacts);
+  }
 
   return (
     <div className='app-container'>
-      <table>
-        <thead>
-        <tr>
-          <th>Name</th>
-          <th>Address</th>
-          <th>Phone Number</th>
-          <th>Email</th>
-        </tr>
-        </thead>
-        <tbody>
-          {/* map function outputs a row for each contact in our contacts array*/}
-          {contacts.map((contact)=>(
+      <form onSubmit={handleEditFormSubmit}>
+        <table>
+          <thead>
           <tr>
-            <td>{contact.fullName}</td>
-            <td>{contact.address}</td>
-            <td>{contact.phoneNumber}</td>
-            <td>{contact.email}</td>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Phone Number</th>
+            <th>Email</th>
+            <th>Actions</th>
           </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {/* map function outputs a row for each contact in our contacts array*/}
+            {contacts.map((contact)=>(
+            <Fragment>
+              {editContactId === contact.id ? (
+                // Left side is child component and right side is Parent component
+                <EditableRow 
+                editFormData = {editFormData} 
+                handleEditFormChange = {handleEditFormChange}
+                handleCancelClick = {handleCancelClick}
+                />
+              ): (
+              <ReadOnlyRow 
+              contact={contact} 
+              handleEditClick = {handleEditClick}
+              handleDeleteClick = {handleDeleteClick}
+              />)}
+            </Fragment>
+            ))}
+          </tbody>
+        </table>
+      </form>
 
       <h2>Add a Contact</h2>
       <form onSubmit={handleAddFormSubmit}>
