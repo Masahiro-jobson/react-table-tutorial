1589a064045e549740fbd7805ca4f8164a4de84c
masahiro <m.togasaki1026@gmail.com>
2025-12-15 16:23:11 +1100

    Made a contact form with map function and useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import { useState } from "react";
 import "./App.css";
+import data from "./mock-data.json";
 
 function App() {
+  // contacts variable holds an array that the initial value is set up
+  // with data parameter in the parentheses of useState.
+  const [contacts, setContacts] = useState(data);
+  // initialize an object that different property has each input.
+  const [addFormData, setAddFormData] = useState({
+    fullName: "",
+    address: "",
+    phoneNumber: "",
+    email: ""
+
+  });
+
+
+
   return (
     <div className='app-container'>
       <table>
@@ -13,18 +29,45 @@ function App() {
         </tr>
         </thead>
         <tbody>
+          {/* map function outputs a row for each contact in our contacts array*/}
+          {contacts.map((contact)=>(
           <tr>
-            <td>Jack Michael</td>
-            <td>3 waterfoot road</td>
-            <td>222=444=555</td>
-            <td>JackMichael343@gmail.com</td>
+            <td>{contact.fullName}</td>
+            <td>{contact.address}</td>
+            <td>{contact.phoneNumber}</td>
+            <td>{contact.email}</td>
           </tr>
+          ))}
         </tbody>
       </table>
+
+      <h2>Add a Contact</h2>
+      <form>
+        <input 
+        type="text" 
+        name="fullName" 
+        required= "required" 
+        placeholder="Enter a name... "/>
+        <input 
+        type="text" 
+        name="address" 
+        required= "required" 
+        placeholder="Enter an address... "/>
+        <input 
+        type="text" 
+        name="phoneNumber" 
+        required= "required" 
+        placeholder="Enter a phone number... "/>
+        <input 
+        type="text" 
+        name="email" 
+        required= "required" 
+        placeholder="Enter an email... "/>
+        <button type="submit">Submit</button>
+      </form>
       
     </div>
   )
 }
 
-export default App
-``
+export default App;
